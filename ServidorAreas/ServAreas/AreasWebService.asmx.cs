e69919b14bc7793bdda396bd6798b2b8f1a77441
Marian Paola <a180124@unach.mx>
2022-10-08 18:50:33 -0500

    Web Metodos y toques finales

--- ServidorAreas/ServAreas/AreasWebService.asmx.cs
+++ ServidorAreas/ServAreas/AreasWebService.asmx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Services;
 
@@ -16,9 +17,26 @@ namespace ServAreas
     // [System.Web.Script.Services.ScriptService]
     public class AreasWebService : System.Web.Services.WebService {
 
-        [WebMethod]
-        public string HelloWorld() {
-            return "Hola a todos";
+        [WebMethod (Description = "Método para calcular el area de un cuadrado")]
+        public decimal cuadrado(decimal lado)
+        {
+            Formulas Cuadrado = new Formulas();
+            Cuadrado.ladoCuadrado = lado;
+            return Cuadrado.cuadrado();
+        }
+
+        [WebMethod (Description = "Método para calcular el area de un triángulo")]
+        public decimal triangulo(decimal baseT, decimal altura)
+        {
+            Formulas Formu = new Formulas();
+            return Formu.triangulo(baseT, altura);
+        }
+
+        [WebMethod(Description = "Método para calcular el area de un círculo")]
+        public decimal circulo(decimal radio)
+        {
+            Formulas Formu = new Formulas();
+            return Formu.circulo(radio);
         }
     }
 }
--- ServidorAreas/ServAreas/Formulas.cs
+++ ServidorAreas/ServAreas/Formulas.cs
@@ -8,9 +8,6 @@ namespace ServAreas
     public class Formulas
     {
         public decimal ladoCuadrado { get; set; }
-        public decimal baseTriangulo { get; set; }
-        public decimal alturaTriangulo { get; set; }
-        public decimal radioCirculo { get; set; }
 
         //Formula para el área del cuadrado (Lado x Lado)
         public decimal cuadrado()
@@ -19,13 +16,13 @@ namespace ServAreas
         }
 
         //Formula para el área del Triangulo (Base x Altura / 2)
-        public decimal triangulo()
+        public decimal triangulo(decimal baseTriangulo, decimal alturaTriangulo)
         {
             return ((baseTriangulo * alturaTriangulo) / 2);
         }
 
         //Formulas para el área del circulo (Pi x Radio al cuadrado)
-        public decimal circulo()
+        public decimal circulo(decimal radioCirculo)
         {
             decimal pi = decimal.Parse("3.1416");
             return (pi * (radioCirculo * radioCirculo));
